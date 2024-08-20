6eb0a6ff67524f380fa835eafc80124b98353739
asus <asus@DESKTOP-OTTG4TB>
2024-08-20 19:47:38 +0300

    Add project files.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class BaseKisi //Bazı özellikler atıyoruz
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void Yazdır() //Base sınıfının ad soya yazdırma metodu
+        {
+            Console.WriteLine("ADINIZ: " + Ad + "SOYADINIZ: " + Soyad);
+        }
+
+    }
+
+    public class Student : BaseKisi //BasePerson'a ait özellikleri Student'a inheritance özellliği ile aktarıyoruz
+    {
+        public int studentNumber { get; set; }
+
+        public void studentInfo ()
+        {
+            Console.WriteLine("ÖĞRENCİ NUMARANIZ: " + studentNumber);
+        }
+
+    }
+
+    public class Teacher : BaseKisi
+    {
+        public string Profession { get; set; }
+
+        public void teacherInfo ()
+        {
+            Console.WriteLine("UZMANLIK ALANINIZ: " + teacherInfo);
+        }
+    }
+
+
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using Inheritance;
+using System;
+
+namespace InheritancePratik
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Ad = "aa";
+            student1.Soyad = "bb";
+            student1.studentNumber = 13;
+
+            Console.WriteLine("Öğrenci Bilgileri:");
+            student1.studentInfo();
+
+
+            Teacher teacher1= new Teacher();
+            teacher1.Ad = "cc";
+            teacher1.Soyad = "dd";
+            teacher1.Profession = "ee";
+
+            Console.WriteLine("Öğretmen Bilgileri:");
+            teacher1.teacherInfo();
+        }
+    }
+}
